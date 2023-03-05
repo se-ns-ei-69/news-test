8f94288d9e0562da911e157a5a7602db1a80085c
Roman <romalasolution@icloud.com>
2023-03-05 02:22:22 +0200

    fix i18next resources

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,24 +1,71 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import translationEN from '../public/locales/en/translation.json';
-import translationUA from '../public/locales/ua/translation.json';
-const resources = {
-  en: {
-    translation: translationEN
-  },
-  ua: {
-    translation: translationUA
-  }
-};
 
 i18n
-  .use(Backend)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
     debug: false,
-    resources
+    resources: {
+      en: {
+        'errors': {
+          'authError': 'Username or password are incorrect.'
+        },
+        'messages': {
+          'emptyList': 'There seems to be nothing here',
+          'loading': 'Loading...'
+        },
+        'buttons': {
+          'login': 'Login',
+          'logout': 'Logout',
+          'delete': 'Delete',
+          'loadMore': 'Read more'
+        },
+        'pages': {
+          'profile': 'PROFILE',
+          'news': 'NEWS',
+          'home': 'HOME'
+        },
+        'modals': {
+          'logout': {
+            'description': 'Are you sure you want to log out?',
+            'buttons': {
+              'agree': 'Log out',
+              'disagree': 'Cancel'
+            }
+          }
+        }
+      },
+      ua: {
+        'errors': {
+          'authError': 'Ім\'я користувача або пароль введено неправильно.'
+        },
+        'messages': {
+          'emptyList': 'Здається тут пусто =(',
+          'loading': 'Завантаження...'
+        },
+        'buttons': {
+          'login': 'Увійти',
+          'logout': 'Вийти',
+          'delete': 'Видалати',
+          'loadMore': 'Читати ще'
+        },
+        'pages': {
+          'profile': 'ПРОФІЛЬ',
+          'news': 'НОВИНИ',
+          'home': 'ГОЛОВНА'
+        },
+        'modals': {
+          'logout': {
+            'description': 'Ви дійсно хочете вийти з акаунта?',
+            'buttons': {
+              'agree': 'Вийти',
+              'disagree': 'Скасувати'
+            }
+          }
+        }
+      }
+    }
   });
 
 
